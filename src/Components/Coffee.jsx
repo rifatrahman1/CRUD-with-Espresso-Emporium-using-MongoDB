9fe08ca8b677cd2e0a5ccf0030df5fec48995cec
Rifat Rahman <ahsan30rifat@gmail.com>
2025-02-26 22:48:55 +0600

    added to first commit

--- src/Components/Coffee.jsx
+++ src/Components/Coffee.jsx
@@ -7,6 +7,7 @@ import Swal from 'sweetalert2'
 
 const Coffee = ({ coffee }) => {
       const { id, name, chef, price, image } = coffee || {};
+      console.log(coffee);
       const handle_delete = () => {
             const swalWithBootstrapButtons = Swal.mixin({
                   customClass: {
